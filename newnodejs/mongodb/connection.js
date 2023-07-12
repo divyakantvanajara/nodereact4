b0e0f710f28c537cd35a55bd999b3dbaeaf7db4a
Your Name <you@example.com>
2023-07-12 21:58:35 +0530

    working with database and collection

--- newnodejs/mongodb/connection.js
+++ newnodejs/mongodb/connection.js
@@ -1,7 +1,7 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = "mongodb://0.0.0.0:27017/reactnode";
- //var url = "mongodb://0.0.0.0/reactnode";
+ //var url = "mongodb://localhost:27017/reactnode";
 MongoClient.connect(url,function(error,database){
     if(error!=null) //if there is error
     {
