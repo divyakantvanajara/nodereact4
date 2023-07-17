df271e9090f3707b8e71e7de8ef53b0bf0311dfa
Your Name <you@example.com>
2023-07-17 10:03:42 +0530

    INSERT UPDATE DELETE AND FETCH

--- newnodejs/mongodb/ex.js
+++ newnodejs/mongodb/ex.js
@@ -1,31 +1,41 @@
-var mongodb =require("mongodb");
-var MongoClient=mongodb.MongoClient;
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+const DATABASENAME="reactnode";
 
-var url ="mongodb://0.0.0.0:27017/";
-const DATABASENAME ="reactnode"
+var url ="mongodb://0.0.0.0:27017";
+
 MongoClient.connect(url,function(error,database){
 
-    if(error !=null)
-    {
-        console.log("error in connection");
-        console.log(error.errmsg);
-    }
-    else
-    {
-      var db =database.db(DATABASENAME);
-     var query ={name:'divyakant'}
-      db.collection('student').deleteMany(query,function(error,result){
+  if(error != null)
+  {
+    console.log("error in connection");
+    console.log(error.errmsg);
+  }
+  else
+  {
+    var db =database.db(DATABASENAME);
+     const condition = 
+     {
+       $and:[
+        {
+          name:{
+            $eq: "The atomic habit",
+          },
+          author: {
+            $eq: "some author",
+          }
+        },
+       ],
+      };
+     
+     var fields_to_skip={_id:0};
 
-        if(error != null)
-        {
-            console.log("error in delete  document");
-            console.log(error.errmsg);
-        }
-        else
-        {
-            console.log(" 1document delete successfully");
-        }
-      })
+     db.collection("books").find(condition,{projection:fields_to_skip}).toArray(function(error,result){
+
+      if(error!=null)
+      console.log(error.errmsg);
+  else 
+      console.log(result);
+     })
     }
-  
-})
+});
